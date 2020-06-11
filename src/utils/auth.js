4c274da5cee8a37c96aedf857a13ee2b62a3ad8e
chryl <cyl5yy412@gmail.com>
2020-06-11 10:31:59 +0800

    修改token

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -11,7 +11,7 @@ export function getToken() {
  *  那么就需要 修改 removeToken(store/module/user.js),查看哪里删除了toekn
  */
 export function setToken(token) {
-  // return Cookies.set(TokenKey, token)
+  // return Cookies.set(TokenKey, token)//默认关闭浏览器过期
   return Cookies.set(TokenKey, token, { expires: 7 })
 }
 
